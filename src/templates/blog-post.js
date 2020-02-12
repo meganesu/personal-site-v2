dbe8b1aa164ba5847ee440baa29a5800dee55332
Megan Sullivan <msulliv@thoughtworks.com>
2020-02-11 23:11:44 -0800

    feat(#5): switch background color of header to navy

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import styles from './blog-post.module.css'
 
 export const query = graphql`
   query (
@@ -24,8 +25,8 @@ export const query = graphql`
 
 const BlogPost = (props) => (
   <Layout>
-    <h1>{ props.data.markdownRemark.frontmatter.title }</h1>
-    <p>{ props.data.markdownRemark.frontmatter.date }</p>
+    <h1 className={styles.title}>{ props.data.markdownRemark.frontmatter.title }</h1>
+    <p className={styles.date}>{ props.data.markdownRemark.frontmatter.date }</p>
     <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html}} />
   </Layout>
 )
