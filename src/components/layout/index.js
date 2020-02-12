c3d1a7efcef5df860c47a59c2595ee67db4d5c81
Megan Sullivan <msulliv@thoughtworks.com>
2020-02-12 09:20:00 -0800

    feat(#5): add orange border around main content

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -12,10 +12,12 @@ const Layout = ({ children }) => (
   <div className={styles.container}>
     <SkipLink link="#main-content">Skip to main content</SkipLink>
     <Header />
-    <main className={styles.main} id="main-content" tabIndex={-1}>
-      {children}
-    </main>
-    <Footer />
+    <div className={styles.mainWrapper}>
+      <main className={styles.main} id="main-content" tabIndex={-1}>
+        {children}
+      </main>
+      <Footer />
+    </div>
   </div>
 )
 
