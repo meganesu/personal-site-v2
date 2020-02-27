16c3e0e64f5f0eacea309e491e1d682bedebe0a5
Megan Sullivan <meganesulli@gmail.com>
2020-02-26 22:30:01 -0800

    chore(#5): format

--- src/components/header/components/social-links/index.js
+++ src/components/header/components/social-links/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faTwitter } from "@fortawesome/free-brands-svg-icons"
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 import styles from "./styles.module.css"
 
@@ -11,17 +11,17 @@ const SocialLinks = () => (
   <ul className={styles.socialLinks}>
     <li>
       <a href="https://github.com/meganesu">
-        <FontAwesomeIcon icon={ faGithub } size="2x" color="white" />
+        <FontAwesomeIcon icon={faGithub} size="2x" color="white" />
       </a>
     </li>
     <li>
       <a href="https://twitter.com/meganesulli">
-        <FontAwesomeIcon icon={ faTwitter } size="2x" color="white" />
+        <FontAwesomeIcon icon={faTwitter} size="2x" color="white" />
       </a>
     </li>
     <li>
       <a href="https://linkedin.com/in/meganesu">
-        <FontAwesomeIcon icon={ faLinkedin } size="2x" color="white" />
+        <FontAwesomeIcon icon={faLinkedin} size="2x" color="white" />
       </a>
     </li>
   </ul>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout/"
-import * as styles from './index.module.css'
-import * as waveImage from '../images/Emoji_u1f44b.svg';
+import * as styles from "./index.module.css"
+import * as waveImage from "../images/Emoji_u1f44b.svg"
 
 const IndexPage = () => (
   <Layout>
@@ -13,12 +13,16 @@ const IndexPage = () => (
       </p>
     </div>
     <p>
-      I’m a software developer. I like <Link to="/projects">building things</Link> and
-      then <Link to="/blog">teaching others</Link> about what I’ve learned.
+      I’m a software developer. I like{" "}
+      <Link to="/projects">building things</Link> and then{" "}
+      <Link to="/blog">teaching others</Link> about what I’ve learned.
     </p>
     <p>
       When I’m not on my computer, I’m a dog volunteer at{" "}
-      <a href="https://www.oaklandanimalservices.org/">Oakland Animal Services</a>.
+      <a href="https://www.oaklandanimalservices.org/">
+        Oakland Animal Services
+      </a>
+      .
     </p>
     <p>Want to get in touch? Send me a message on Twitter!</p>
   </Layout>
