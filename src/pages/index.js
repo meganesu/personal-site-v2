7272d866561fb975bf07c6a665bf140df49ed796
Megan Sullivan <meganesulli@gmail.com>
2020-02-26 22:01:58 -0800

    feat(#5): update home page content to match style guide

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,26 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout/"
+import * as styles from './index.module.css'
+import * as waveImage from '../images/Emoji_u1f44b.svg';
 
 const IndexPage = () => (
   <Layout>
-    <p>Here's a little about me:</p>
-    <ul>
-      <li>I'm currently a full-time software developer</li>
-      <li>I love dogs more than all other things on the planet</li>
-      <li>My life goal is to someday have my own homestead</li>
-      <li>
-        In my spare time, I volunteer at{" "}
-        <a href="https://www.oaklandanimalservices.org/">
-          Oakland Animal Services
-        </a>
-        . Looking to add a furry friend to your family? Come by!
-      </li>
-    </ul>
-    <p>Want to get in touch? Send me a message on LinkedIn!</p>
+    <div>
+      <p className={styles.greeting}>
+        <img src={waveImage} className={styles.wave} alt="wave emoji" />
+        Hey there, I'm <span className={styles.name}>Megan</span>!
+      </p>
+    </div>
+    <p>
+      I’m a software developer. I like <Link to="/projects">building things</Link> and
+      then <Link to="/blog">teaching others</Link> about what I’ve learned.
+    </p>
+    <p>
+      When I’m not on my computer, I’m a dog volunteer at{" "}
+      <a href="https://www.oaklandanimalservices.org/">Oakland Animal Services</a>.
+    </p>
+    <p>Want to get in touch? Send me a message on Twitter!</p>
   </Layout>
 )
 
