6bbb58224986bfe4e2f683c5896f42ed37bb0741
Megan Sullivan <meganesulli@gmail.com>
2020-03-04 10:01:36 -0800

    feat(#2): add and configure Google Analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-159656850-1",
+        head: false, // Place the tracking script in the head (instead of in the body)
+        anonymize: true, // (optional) Anonymize the IP address of the sender
+        respectDNT: true, // (optional) Respect user's Do Not Track setting
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "src",
