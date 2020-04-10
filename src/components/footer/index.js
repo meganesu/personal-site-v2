ee0fa8593c6bf50acf8ecd247cdbaeac414d7cb8
Megan Sullivan <meganesulli@gmail.com>
2020-04-10 10:04:20 -0700

    fix(a11y): use footer element instead of div in footer component

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,12 +4,12 @@ import { Link } from "gatsby"
 import styles from "./styles.module.css"
 
 const Footer = () => (
-  <div className={styles.footer}>
+  <footer className={styles.footer}>
     <p>{`Made by Megan Sullivan © 2019 - ${new Date().getFullYear()}`}</p>
     <p>
       <Link to="/privacy">Privacy Policy</Link>
     </p>
-  </div>
+  </footer>
 )
 
 export default Footer
