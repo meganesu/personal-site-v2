b291c764a81213118055c5ab5cdfb2fa9e1a2c04
Megan Sullivan <meganesulli@gmail.com>
2020-07-19 15:02:38 -0700

    feat(styles): add new layout for desktop breakpoint #13

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,10 @@ import * as waveImage from "../images/Emoji_u1f44b.svg"
 
 const IndexPage = () => (
   <Layout pageTitle="Home | Megan Sullivan">
-    <div>
-      <p className={styles.greeting}>
-        <img src={waveImage} className={styles.wave} alt="wave emoji" />
-        Hey there, I'm <span className={styles.name}>Megan</span>!
-      </p>
-    </div>
+    <p className={styles.greeting}>
+      <img src={waveImage} className={styles.wave} alt="wave emoji" />
+      Hey there, I'm <span className={styles.name}>Megan</span>!
+    </p>
     <p>
       I’m a software developer. I like{" "}
       <Link to="/projects">building things</Link> and then{" "}
