acc1000ed723196045e33d75af5c78f50a1cbe3b
Megan Sullivan <meganesulli@gmail.com>
2020-07-26 01:38:41 -0700

    fix: unescape quotes in deploy script

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -1,8 +1,9 @@
 CUSTOM_DOMAIN="meganesulli.com"
 
-echo \"Commit: $TRAVIS_COMMIT\nMessage: $TRAVIS_COMMIT_MESSAGE\"
+echo "Commit: $TRAVIS_COMMIT"
+echo "Message: $TRAVIS_COMMIT_MESSAGE"
 
 # Create CNAME file for hosting GitHub Pages site at a custom domain
 echo $CUSTOM_DOMAIN > public/CNAME
 
-gh-pages -d public -b master -r https://$GITHUB_TOKEN@github.com/meganesu/meganesu.github.io.git -m \"$TRAVIS_COMMIT_MESSAGE ($TRAVIS_COMMIT)\"
+npx gh-pages -d public -b master -r "https://$GITHUB_TOKEN@github.com/meganesu/meganesu.github.io.git" -m "$TRAVIS_COMMIT_MESSAGE ($TRAVIS_COMMIT)"
