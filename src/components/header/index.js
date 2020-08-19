974d12866dd6c5950bb4f1963a13d5094bcb4dfe
Megan Sullivan <meganesulli@gmail.com>
2020-08-19 14:16:25 -0700

    feat: add profile picture to header for desktop breakpoint #13

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,9 +3,11 @@ import { Link } from "gatsby"
 import NavBar from "./components/nav-bar/"
 import SocialLinks from "./components/social-links/"
 import styles from "./styles.module.css"
+import * as profilePicture from "../../images/meganesulli.jpg"
 
 const Header = () => (
   <header className={styles.header}>
+    <img src={profilePicture} className={styles.profilePicture} />
     <p className={styles.title}>
       <Link to="/">
         Megan Sullivan
