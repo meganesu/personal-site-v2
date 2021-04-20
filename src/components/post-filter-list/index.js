9df5a54968894c7cc54a66259a389c4e2b283492
Megan Sullivan <meganesulli@gmail.com>
2021-04-19 21:56:38 -0700

    feat(wip): connect tag filters to list of posts

--- src/components/post-filter-list/index.js
+++ src/components/post-filter-list/index.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import Chip from '../chip'
-import {list as listStyles, listItem as listItemStyles} from './styles.module.css'
+import React from "react"
+import Chip from "../chip"
+import {
+  list as listStyles,
+  listItem as listItemStyles,
+} from "./styles.module.css"
 
-const PostFilterList = ({filters, color}) => {
+const PostFilterList = ({ filters, color, onAdd, onRemove }) => {
   return (
     <ul className={listStyles}>
-      {
-        filters.map(tag => (
-          <li className={listItemStyles}>
-            <Chip text={tag} color={color} />
-          </li>
-        ))
-      }
+      {filters.map((tag) => (
+        <li className={listItemStyles}>
+          <Chip text={tag} color={color} onAdd={onAdd} onRemove={onRemove} />
+        </li>
+      ))}
     </ul>
   )
 }
