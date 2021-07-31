13f5d5ff4769598404aadad9eabfa660111ea6d3
Megan Sullivan <meganesulli@gmail.com>
2021-07-31 14:59:40 -0700

    feat: style collapsible component

--- src/components/collapsible/index.js
+++ src/components/collapsible/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import {
+  details as detailsStyles,
+  summary as summaryStyles,
+} from './styles.module.css'
 
 const Collapsible = ({ summary, children }) => (
-  <details>
-    <summary>
+  <details className={detailsStyles}>
+    <summary className={summaryStyles}>
       {summary}
     </summary>
     {children}
