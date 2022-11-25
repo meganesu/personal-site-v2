3c8bba24cc83aa3969e301391c858b7f1d97dd45
Megan Sullivan <meganesulli@gmail.com>
2022-11-24 18:20:44 -0800

    fix: add redirect for generate-gb-opcodes (to GitHub Pages)

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     favicon: `images/favicon.png`
   },
   plugins: [
+    "gatsby-plugin-netlify",
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-image",
