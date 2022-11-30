a9c0b54c0a534b55bd1bebd208ab95ead052c8a5
Megan Sullivan <meganesulli@gmail.com>
2022-11-29 16:16:19 -0800

    chore: migrate from gatsby-plugin-google-analytics to gatsby-plugin-google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,14 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Megan Sullivan's Personal Website`,
     siteUrl: `https://meganesulli.com`,
     description: `The personal website for Megan Sullivan, a software developer and educator.`,
     image: `images/meganesulli-default-social-card.png`,
-    favicon: `images/favicon.png`
+    favicon: `images/favicon.png`,
   },
   plugins: [
     "gatsby-plugin-netlify",
@@ -15,7 +19,7 @@ module.exports = {
       resolve: "gatsby-plugin-mdx",
       options: {
         defaultLayouts: {
-          default: require.resolve("./src/components/layout")
+          default: require.resolve("./src/components/layout"),
         },
         gatsbyRemarkPlugins: [
           {
@@ -23,24 +27,26 @@ module.exports = {
             options: {
               className: "header-anchor",
               enableCustomId: true,
-            }
+            },
           },
           {
             resolve: "gatsby-remark-images",
             options: {
               maxWidth: 800,
-            }
-          }
-        ]
-      }
+            },
+          },
+        ],
+      },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "UA-159656850-1",
-        head: false, // Place the tracking script in the head (instead of in the body)
-        anonymize: true, // (optional) Anonymize the IP address of the sender
-        respectDNT: true, // (optional) Respect user's Do Not Track setting
+        trackingIds: [process.env.GOOGLE_ANALYTICS_4_TRACKING_ID],
+        pluginConfig: {
+          head: false, // Place the tracking script in the head (instead of in the body)
+          anonymize: true, // (optional) Anonymize the IP address of the sender
+          respectDNT: true, // (optional) Respect user's Do Not Track setting
+        },
       },
     },
     {
@@ -58,7 +64,7 @@ module.exports = {
             title: `Megan Sullivan's Blog RSS Feed`,
             output: `/rss.xml`,
             serialize: ({ query: { site, allMdx } }) => {
-              return allMdx.nodes.map(node => {
+              return allMdx.nodes.map((node) => {
                 return Object.assign({}, node.frontmatter, {
                   url: `${site.siteMetadata.siteUrl}/blog/${node.slug}`,
                   guid: `${site.siteMetadata.siteUrl}/blog/${node.slug}`,
@@ -81,9 +87,9 @@ module.exports = {
                 }
               }
             `,
-          }
+          },
         ],
-      }
+      },
     },
   ],
 }
--- src/components/header/components/social-links/index.js
+++ src/components/header/components/social-links/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { OutboundLink } from "gatsby-plugin-google-analytics"
+import { OutboundLink } from "gatsby-plugin-google-gtag"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
@@ -12,17 +12,26 @@ import { socialLinks as socialLinksStyles } from "./styles.module.css"
 const SocialLinks = () => (
   <ul className={socialLinksStyles}>
     <li>
-      <OutboundLink href="https://github.com/meganesu" aria-label="Megan's GitHub profile">
+      <OutboundLink
+        href="https://github.com/meganesu"
+        aria-label="Megan's GitHub profile"
+      >
         <FontAwesomeIcon icon={faGithub} size="2x" color="white" />
       </OutboundLink>
     </li>
     <li>
-      <OutboundLink href="https://twitter.com/meganesulli" aria-label="Megan's Twitter profile">
+      <OutboundLink
+        href="https://twitter.com/meganesulli"
+        aria-label="Megan's Twitter profile"
+      >
         <FontAwesomeIcon icon={faTwitter} size="2x" color="white" />
       </OutboundLink>
     </li>
     <li>
-      <OutboundLink href="https://linkedin.com/in/meganesu" aria-label="Megan's LinkedIn profile">
+      <OutboundLink
+        href="https://linkedin.com/in/meganesu"
+        aria-label="Megan's LinkedIn profile"
+      >
         <FontAwesomeIcon icon={faLinkedin} size="2x" color="white" />
       </OutboundLink>
     </li>
