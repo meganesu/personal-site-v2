a26dab8f081743f8101612fd5c0190d2fd6a8276
Megan Sullivan <meganesulli@gmail.com>
2022-11-29 16:16:19 -0800

    chore: migrate from gatsby-plugin-google-analytics to gatsby-plugin-google-gtag

--- src/components/ko-fi-button/index.js
+++ src/components/ko-fi-button/index.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import { OutboundLink } from 'gatsby-plugin-google-analytics'
-import { container as containerStyles, koFiLogo as koFiLogoStyles, chip as chipStyles } from './styles.module.css'
+import React from "react"
+import { OutboundLink } from "gatsby-plugin-google-gtag"
+import {
+  container as containerStyles,
+  koFiLogo as koFiLogoStyles,
+  chip as chipStyles,
+} from "./styles.module.css"
 
 const KoFiButton = () => {
   return (
-    <OutboundLink href="https://ko-fi.com/meganesulli" className={`${chipStyles} ${containerStyles}`}>
-        <img
-          src="https://storage.ko-fi.com/cdn/cup-border.png"
-          className={koFiLogoStyles }
-          alt="Ko-fi logo"
-        />
-        Support me
+    <OutboundLink
+      href="https://ko-fi.com/meganesulli"
+      className={`${chipStyles} ${containerStyles}`}
+    >
+      <img
+        src="https://storage.ko-fi.com/cdn/cup-border.png"
+        className={koFiLogoStyles}
+        alt="Ko-fi logo"
+      />
+      Support me
     </OutboundLink>
   )
 }
