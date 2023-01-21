4da055d884138557aafde9a0870cdb982db47de1
Megan Sullivan <meganesulli@gmail.com>
2023-01-20 16:49:51 -0800

    chore: fix lint

--- src/components/mailing-list-signup-form/index.js
+++ src/components/mailing-list-signup-form/index.js
@@ -1,87 +1,76 @@
-import React from 'react'
+import React from "react"
 
-import CallToActionBox from '../call-to-action';
+import CallToActionBox from "../call-to-action"
 
 export const TemplateMailingListSignupForm = () => {
   return (
-    <>
-    <script src="https://f.convertkit.com/ckjs/ck.5.js">
-    </script>
-    <form
-      action="https://app.convertkit.com/forms/4003750/subscriptions"
-      class="seva-form formkit-form"
-      method="post"
-      data-sv-form="4003750"
-      data-uid="e7e1f92800"
-      data-format="inline"
-      data-version="5"
-      data-options="{&quot;settings&quot;:{&quot;after_subscribe&quot;:{&quot;action&quot;:&quot;message&quot;,&quot;success_message&quot;:&quot;Success! Now check your email to confirm your subscription.&quot;,&quot;redirect_url&quot;:&quot;&quot;},&quot;analytics&quot;:{&quot;google&quot;:null,&quot;fathom&quot;:null,&quot;facebook&quot;:null,&quot;segment&quot;:null,&quot;pinterest&quot;:null,&quot;sparkloop&quot;:null,&quot;googletagmanager&quot;:null},&quot;modal&quot;:{&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;powered_by&quot;:{&quot;show&quot;:true,&quot;url&quot;:&quot;https://convertkit.com/features/forms?utm_campaign=poweredby&amp;utm_content=form&amp;utm_medium=referral&amp;utm_source=dynamic&quot;},&quot;recaptcha&quot;:{&quot;enabled&quot;:false},&quot;return_visitor&quot;:{&quot;action&quot;:&quot;show&quot;,&quot;custom_content&quot;:&quot;&quot;},&quot;slide_in&quot;:{&quot;display_in&quot;:&quot;bottom_right&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;sticky_bar&quot;:{&quot;display_in&quot;:&quot;top&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15}},&quot;version&quot;:&quot;5&quot;}"
-      min-width="400 500 600 700 800"
-    >
-      <div
-        class="formkit-background"
+    <CallToActionBox>
+      <script src="https://f.convertkit.com/ckjs/ck.5.js" />
+      <form
+        action="https://app.convertkit.com/forms/4003750/subscriptions"
+        class="seva-form formkit-form"
+        method="post"
+        data-sv-form="4003750"
+        data-uid="e7e1f92800"
+        data-format="inline"
+        data-version="5"
+        data-options='{"settings":{"after_subscribe":{"action":"message","success_message":"Success! Now check your email to confirm your subscription.","redirect_url":""},"analytics":{"google":null,"fathom":null,"facebook":null,"segment":null,"pinterest":null,"sparkloop":null,"googletagmanager":null},"modal":{"trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"powered_by":{"show":true,"url":"https://convertkit.com/features/forms?utm_campaign=poweredby&amp;utm_content=form&amp;utm_medium=referral&amp;utm_source=dynamic"},"recaptcha":{"enabled":false},"return_visitor":{"action":"show","custom_content":""},"slide_in":{"display_in":"bottom_right","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15},"sticky_bar":{"display_in":"top","trigger":"timer","scroll_percentage":null,"timer":5,"devices":"all","show_once_every":15}},"version":"5"}'
+        min-width="400 500 600 700 800"
       >
-      </div>
-      <div data-style="minimal">
-        <div
-          class="formkit-header"
-          data-element="header"
-        >
-        <h2>Join the Newsletter</h2>
-        </div>
-        <div
-        class="formkit-subheader"
-        data-element="subheader">
-          <p>Subscribe to get email updates about new content!</p>
-        </div>
-        <ul class="formkit-alert formkit-alert-error"
-        data-element="errors"
-        data-group="alert">
+        <div class="formkit-background"></div>
+        <div data-style="minimal">
+          <div class="formkit-header" data-element="header">
+            <h2>Join the Newsletter</h2>
+          </div>
+          <div class="formkit-subheader" data-element="subheader">
+            <p>Subscribe to get email updates about new content!</p>
+          </div>
+          <ul
+            class="formkit-alert formkit-alert-error"
+            data-element="errors"
+            data-group="alert"
+          >
           </ul>
-        <div data-element="fields"
-        data-stacked="true"
-        class="seva-fields formkit-fields">
-          <div class="formkit-field">
-          <input
-            class="formkit-input"
-            aria-label="First Name"
-            name="fields[first_name]"
-            required=""
-            placeholder="First Name"
-            type="text"
-          />
+          <div
+            data-element="fields"
+            data-stacked="true"
+            class="seva-fields formkit-fields"
+          >
+            <div class="formkit-field">
+              <label for="first-name">First name</label>
+              <input
+                type="text"
+                id="first-name"
+                name="fields[first_name]"
+                class="formkit-input"
+                required=""
+              />
+            </div>
+            <div class="formkit-field">
+              <label for="email-address">Email address</label>
+              <input
+                type="email"
+                id="email-address"
+                name="email_address"
+                class="formkit-input"
+                required=""
+              />
+            </div>
+            <button data-element="submit" class="formkit-submit formkit-submit">
+              <div class="formkit-spinner">
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+              <span class="">Subscribe</span>
+            </button>
           </div>
-        <div class="formkit-field">
-          <input
-            class="formkit-input"
-            name="email_address"
-            aria-label="Email Address"
-            placeholder="Email Address"
-            required=""
-            type="email"
-          />
-          </div>
-        <button data-element="submit"
-        class="formkit-submit formkit-submit"
-        >
-          <div class="formkit-spinner">
-          <div>
-          </div>
-        <div>
-          </div>
-        <div>
+          <div class="formkit-guarantee" data-element="guarantee">
+            <p>No spam. Unsubscribe at any time.</p>
           </div>
         </div>
-        <span class="">Subscribe</span>
-        </button>
-        </div>
-        <div class="formkit-guarantee"
-        data-element="guarantee">
-          <p>No spam. Unsubscribe at any time.</p>
-        </div>
-        </div>
-        </form>
-        </>
+      </form>
+    </CallToActionBox>
   )
 }
 
