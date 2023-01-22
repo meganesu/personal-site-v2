e8b4e992093c900e6433f38ba65f95a7ed90af62
Megan Sullivan <meganesulli@gmail.com>
2023-01-22 13:03:18 -0800

    wip: add empty tests, for testing jest setup

---
+++ src/components/code-block/index.test.js
@@ -0,0 +1,13 @@
+describe('CodeBlock component', () => {
+  it('renders', () => {
+
+  });
+
+  it('renders with file title', () => {
+    console.log('running a test')
+  });
+
+  it('renders with language tag', () => {
+
+  });
+});
