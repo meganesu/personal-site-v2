8a20a52cfc2f5be3809ad451d3a542df2046c67c
Megan Sullivan <meganesulli@gmail.com>
2023-01-25 14:19:12 -0800

    fix: convert node_modules used in tests from ESM to CommonJS before running tests
    - @mdx-js/mdx
    - remark-mdx-code-meta

--- src/components/code-block/index.test.js
+++ src/components/code-block/index.test.js
@@ -1,28 +1,11 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 
+import * as runtime from 'react/jsx-runtime'
+import { evaluate } from "../../../vendor/mdx"
+import remarkMdxCodeMeta from '../../../vendor/remark-mdx-code-meta';
+
 import CodeBlock from "."
-
-// const Title = () => (
-//   <div>This is my title!</div>
-// )
-
-test("Displays the correct title", () => {
-  render(
-    <CodeBlock>
-      ```js title="my-test-file.js"
-      const test = "hey there!";
-      ```
-    </CodeBlock>
-  )
-
-  // render(<Title />)
-
-  // const element = screen.getByText("This is my title!")
-  const element = screen.getByText("my-test-file.js")
-
-  expect(element).toBeDefined()
-})
 
 describe('CodeBlock component', () => {
   it('renders', () => {
@@ -30,7 +13,30 @@ describe('CodeBlock component', () => {
   });
 
   it('renders with file title', () => {
-    
+    const mdFencedCodeBlock = `
+      \`\`\`js title="my-test-file.js"
+      const test = "hey there!"
+      \`\`\`
+    `
+
+    const mdxModule = await evaluate(
+      mdFencedCodeBlock,
+      {
+        ...runtime,
+        development: false,
+        remarkPlugins: [remarkMdxCodeMeta],
+      }
+    )
+
+    const MdxContent = mdxModule.default
+
+    render(
+      <MdxContent components={{pre: CodeBlock}} />
+    )
+
+    const element = screen.getByText("my-test-file.js")
+
+    expect(element).toBeDefined()
   });
 
   it('renders with language tag', () => {
