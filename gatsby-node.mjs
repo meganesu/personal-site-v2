ada1d7db2534297ecda7264992929ebab96a3d48
Megan Sullivan <meganesulli@gmail.com>
2023-01-28 18:28:30 -0800

    fix: generate blog post pages using gatsby-node createPages & template (instead of file system routes)
    - File system routes don't let you filter out nodes, so pages were being created for all MDX nodes (including those in src/pages), not just the ones in the /blog/ directory

--- gatsby-node.mjs
+++ gatsby-node.mjs
@@ -1,6 +1,8 @@
 import readingTime from "reading-time"
 import slugify from "@sindresorhus/slugify"
 
+import path from "path";
+
 export const onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
@@ -36,4 +38,57 @@ export const onCreateNode = ({ node, actions, getNode }) => {
       },
     })
   }
+}
+
+export const createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  // Query for MDX nodes in the /blog/ directory
+  const result = await graphql(
+    `
+      {
+        allMdx(
+          filter: {
+            internal: {
+              # Example contentFilePath values:
+              # For local development: /Users/megansullivan/Documents/personal-projects/personal-site-v2/blog/managing-focus-with-react-and-jest/index.mdx
+              # On Netlify: /opt/build/repo/blog/why-use-graphql-sketchnote/index.mdx
+              contentFilePath: { regex: "/(personal-site-v2|build/repo)/blog//" }
+            }
+          }
+        ) {
+          nodes {
+            id
+            fields {
+              slug
+            }
+            internal {
+              contentFilePath
+            }
+          }
+        }
+      }
+    `
+  )
+
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query to create pages for MDX blog posts.`)
+    return
+  }
+
+  // Create pages for each MDX file
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+  result.data.allMdx.nodes.forEach(node => {
+    console.log('NODE:', { node })
+    const slug = node.fields.slug
+
+    createPage({
+      path: `/blog${slug}`,
+      component: `${blogPostTemplate}?__contentFilePath=${node.internal.contentFilePath}`,
+      context: {
+        id: node.id,
+      }
+    })
+  })
 }
