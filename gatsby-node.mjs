69fb285e78f1ce1ec5dc316ff8f750ddb8758e7f
Megan Sullivan <meganesulli@gmail.com>
2023-01-28 16:43:41 -0800

    wip: fix MDX slugs by only creating slug and timeToRead fields for MDX nodes in the /blog/ directory
    Still getting the following error (and a page created for route /blog/{mdx.fields__slug})
    
     ERROR #gatsby-plugin-page-creator_12104  PLUGIN
    
    PageCreator: Could not find a value in the node for blog/{mdx.fields__slug}.js. Please make
    sure that the syntax is correct and supported.
    
    See our docs page for more info on this error:
    https://www.gatsbyjs.com/docs/file-system-route-api/\#syntax-collection-routes

--- gatsby-node.mjs
+++ gatsby-node.mjs
@@ -1,10 +1,31 @@
 import readingTime from "reading-time"
 import slugify from "@sindresorhus/slugify"
 
-export const onCreateNode = ({ node, actions }) => {
+export const onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === "Mdx") {
+    // Add additional fields for blog post pages
+
+    // Skip any MDX nodes that aren't in the /blog/ directory
+    const parentFileNode = getNode(node.parent)
+    if (!parentFileNode.absolutePath.includes("/blog/" + parentFileNode.relativePath)) {
+      return;
+    }
+
+    // Add slug field to be used by file system routes for `{mdx.fields__slug}.js` blog post pages
+    let slug = parentFileNode.name
+    if (slug === "index") {
+      slug = parentFileNode.relativeDirectory
+    }
+
+    createNodeField({
+      node,
+      name: "slug",
+      value: `/${slugify(slug)}`,
+    })
+
+    // Add field for estimated reading time
     const fileReadingTime = readingTime(node.body)
     createNodeField({
       node,
@@ -13,12 +34,6 @@ export const onCreateNode = ({ node, actions }) => {
         ...fileReadingTime,
         minutesRoundedUp: Math.ceil(fileReadingTime.minutes)
       },
-    })
-
-    createNodeField({
-      node,
-      name: "slug",
-      value: `/${slugify(node.frontmatter.title)}`,
     })
   }
 }
