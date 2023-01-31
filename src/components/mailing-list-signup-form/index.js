80f2f8bf926cb8f99e06944776742d09c13a1c1d
Megan Sullivan <meganesulli@gmail.com>
2023-01-30 22:26:17 -0800

    feat: add form styling for happy path

--- src/components/mailing-list-signup-form/index.js
+++ src/components/mailing-list-signup-form/index.js
@@ -3,6 +3,12 @@ import { useForm } from "react-hook-form"
 
 import CallToActionBox from "../call-to-action"
 
+import {
+  formFieldWrapper as formFieldWrapperStyles,
+  formField as formFieldStyles,
+  submit as submitStyles,
+} from "./styles.module.css"
+
 const MailingListSignupForm = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
 
@@ -28,20 +34,21 @@ const MailingListSignupForm = () => {
   return (
     <CallToActionBox>
       <h2>Join the newsletter!</h2>
-      <p>Subscribe to get email updates about new content!</p>
-      <p>No spam. Unsubscribe at any time.</p>
+      <p>Subscribe to get email updates about new content! (No spam. Unsubscribe at any time.)</p>
       <form
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div>
-          <label htmlFor="name">First name</label>
-          <input type="text" name="first-name" id="name" {...register("first_name") } />
+        <div className={formFieldWrapperStyles}>
+          <div className={formFieldStyles}>
+            <label htmlFor="name">First name</label>
+            <input type="text" name="first-name" id="name" {...register("first_name") } />
+          </div>
+          <div className={formFieldStyles}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" { ...register ("email", {required: true}) } />
+          </div>
         </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" { ...register ("email", {required: true}) } />
-        </div>
-        <input type="submit" value="Submit" />
+        <input className={submitStyles} type="submit" value="Submit" />
       </form>
     </CallToActionBox>
   )
