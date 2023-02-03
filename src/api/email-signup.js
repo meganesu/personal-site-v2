9c6170a8df2f174231ef1c5920556ec0cf944b12
Megan Sullivan <meganesulli@gmail.com>
2023-02-02 23:43:44 -0800

    feat: add success/error messages on mailing list form submit

--- src/api/email-signup.js
+++ src/api/email-signup.js
@@ -1,10 +1,11 @@
 export default async (req, res) => {
-  console.log(`submitted form`, req.body)
+  console.log(`submitted form:`, req.body)
 
   const endpoint = `https://api.convertkit.com/v3/forms/${process.env.CONVERTKIT_FORM_ID}/subscribe`
 
   console.log("calling endpoint:", endpoint)
-  let data = await fetch(endpoint, {
+
+  let response = await fetch(endpoint, {
     method: `POST`,
     body: JSON.stringify({
       api_key: process.env.CONVERTKIT_API_KEY,
@@ -15,10 +16,25 @@ export default async (req, res) => {
     },
   })
 
-  console.log(`initial response from ConvertKit:`, data)
+  const statusFromConvertKit = response.status
 
-  data = await data.json()
-  console.log(`json response from ConvertKit:`, data)
+  if (statusFromConvertKit === 200) {
+    res.json({
+      status: 200,
+    })
+    return
+  }
 
-  res.json(`ok`)
+  // If you get here, something went wrong
+  // - Log the request body & error
+  // - Send back the response status code & responseBody error & message
+  console.log(`initial response from ConvertKit:`, response)
+
+  const responseBody = await response.json()
+  console.log(`after response.json():`, responseBody)
+
+  res.json({
+    status: statusFromConvertKit,
+    ...responseBody,
+  })
 }
--- src/components/mailing-list-signup-form/index.js
+++ src/components/mailing-list-signup-form/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { useForm } from "react-hook-form"
 
 import CallToActionBox from "../call-to-action"
@@ -7,13 +7,22 @@ import {
   formFieldWrapper as formFieldWrapperStyles,
   formField as formFieldStyles,
   submit as submitStyles,
+  successMessage as successMessageStyles,
+  errorMessage as errorMessageStyles,
 } from "./styles.module.css"
 
 const MailingListSignupForm = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
 
-  const onSubmit = data => {
-    console.log('DATA IN ONSUBMIT', data)
+  const [success, setSuccess] = useState(false)
+  const [error, setError] = useState(false)
+
+  const onSubmit = (data) => {
+    console.log("DATA IN ONSUBMIT", data)
     fetch(`/api/email-signup`, {
       method: `POST`,
       body: JSON.stringify(data),
@@ -21,35 +30,65 @@ const MailingListSignupForm = () => {
         "content-type": `application/json`,
       },
     })
-      .then(res => res.json())
-      .then(body => {
+      .then((res) => res.json())
+      .then((body) => {
         console.log(`response from API:`, body)
+        if (body.status === 200) {
+          setSuccess(true)
+        }
+        else {
+          setError(true)
+          console.log("Error from /api/email-signup:", body)
+        }
       })
   }
 
-  if ( Object.keys(errors).length !== 0 ) {
-    console.log('ERRORS:', errors)
+  if (Object.keys(errors).length !== 0) {
+    console.log("ERRORS:", errors)
   }
 
   return (
     <CallToActionBox>
       <h2>Join the newsletter!</h2>
-      <p>Subscribe to get email updates about new content! (No spam. Unsubscribe at any time.)</p>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className={formFieldWrapperStyles}>
-          <div className={formFieldStyles}>
-            <label htmlFor="name">First name</label>
-            <input type="text" name="first-name" id="name" {...register("first_name") } />
+      <p>
+        Subscribe to get email updates about new content! (No spam. Unsubscribe
+        at any time.)
+      </p>
+      {success ? (
+        <p className={successMessageStyles}>
+          Thanks for subscribing! Check your inbox for a confirmation email.
+        </p>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          { error && (
+            <p className={errorMessageStyles}>
+              Oops, something's wrong on my end. Please try again later.
+            </p>
+          )}
+          <div className={formFieldWrapperStyles}>
+            <div className={formFieldStyles}>
+              <label htmlFor="name">Preferred name</label>
+              <input
+                type="text"
+                name="preferred-name"
+                id="name"
+                {...register("first_name")}
+              />
+            </div>
+            <div className={formFieldStyles}>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                {...register("email", { required: true })}
+              />
+            </div>
           </div>
-          <div className={formFieldStyles}>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" { ...register ("email", {required: true}) } />
-          </div>
-        </div>
-        <input className={submitStyles} type="submit" value="Submit" />
-      </form>
+          <input className={submitStyles} type="submit" value="Submit" />
+        </form>
+      )}
     </CallToActionBox>
   )
 }
