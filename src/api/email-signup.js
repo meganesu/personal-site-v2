1263ab22f7d32a7422a0caad012efe13d1e61bd6
Megan Sullivan <meganesulli@gmail.com>
2023-02-03 23:35:06 -0800

    fix: wrap serverless function in try/catch, in case of unexpected errors

--- src/api/email-signup.js
+++ src/api/email-signup.js
@@ -1,58 +1,68 @@
 import fetch from "node-fetch"
 
 export default async (req, res) => {
-  console.log(`submitted form:`, req.body)
+  try {
+    console.log(`submitted form:`, req.body)
 
-  // For security, only pull out the expected fields from the request
-  const formData = {
-    first_name: req.body.preferred_name,
-    email: req.body.email,
-  }
+    // For security, only pull out the expected fields from the request
+    const formData = {
+      first_name: req.body.preferred_name,
+      email: req.body.email,
+    }
 
-  // Don't call the ConvertKit API if there's no email
-  if (!formData.email) {
-    res.json({
-      status: 406,
-      error: "Missing required field",
-      message: "/api/email-signup called without required email parameter"
+    // Don't call the ConvertKit API if there's no email
+    if (!formData.email) {
+      res.json({
+        status: 406,
+        error: "Missing required field",
+        message: "/api/email-signup called without required email parameter"
+      })
+      return
+    }
+
+    const endpoint = `https://api.convertkit.com/v3/forms/${process.env.CONVERTKIT_FORM_ID}/subscribe`
+
+    console.log("calling endpoint:", endpoint)
+
+    let response = await fetch(endpoint, {
+      method: `POST`,
+      body: JSON.stringify({
+        api_key: process.env.CONVERTKIT_API_KEY,
+        ...formData
+      }),
+      headers: {
+        "content-type": `application/json`,
+      },
     })
-    return
-  }
 
-  const endpoint = `https://api.convertkit.com/v3/forms/${process.env.CONVERTKIT_FORM_ID}/subscribe`
+    const statusFromConvertKit = response.status
 
-  console.log("calling endpoint:", endpoint)
+    if (statusFromConvertKit === 200) {
+      res.json({
+        status: 200,
+      })
+      return
+    }
 
-  let response = await fetch(endpoint, {
-    method: `POST`,
-    body: JSON.stringify({
-      api_key: process.env.CONVERTKIT_API_KEY,
-      ...formData
-    }),
-    headers: {
-      "content-type": `application/json`,
-    },
-  })
+    // If you get here, something went wrong
+    // - Log the error
+    // - Send back the response status code & responseBody error & message
+    console.log(`initial response from ConvertKit:`, response)
 
-  const statusFromConvertKit = response.status
+    const responseBody = await response.json()
+    console.log(`after response.json():`, responseBody)
 
-  if (statusFromConvertKit === 200) {
     res.json({
-      status: 200,
+      status: statusFromConvertKit,
+      ...responseBody,
     })
-    return
+  } catch(error) {
+    console.log('error:', error)
+
+    res.json({
+      status: 500,
+      message: "Internal server error in /api/email-signup",
+      error: { message: error.message, stack: error.stack }
+    })
   }
-
-  // If you get here, something went wrong
-  // - Log the error
-  // - Send back the response status code & responseBody error & message
-  console.log(`initial response from ConvertKit:`, response)
-
-  const responseBody = await response.json()
-  console.log(`after response.json():`, responseBody)
-
-  res.json({
-    status: statusFromConvertKit,
-    ...responseBody,
-  })
 }
