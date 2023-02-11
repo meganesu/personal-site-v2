cfbeeb03735cd89b1d6a2d5adddd9cae26c0dda6
Megan Sullivan <meganesulli@gmail.com>
2023-02-11 00:36:18 -0800

    feat: adjust navbar social links to change icon cover on hover
    - also fixes height of icon link to fit the whole icon

--- src/components/header/components/social-links/index.js
+++ src/components/header/components/social-links/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import { OutboundLink } from "gatsby-plugin-google-gtag"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { faTwitter } from "@fortawesome/free-brands-svg-icons"
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { faGithub, faTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
-import { socialLinks as socialLinksStyles } from "./styles.module.css"
+import {
+  socialLinks as socialLinksStyles,
+  link as linkStyles,
+  icon as iconStyles,
+} from "./styles.module.css"
 
 const SocialLinks = () => (
   <ul className={socialLinksStyles}>
@@ -15,24 +17,27 @@ const SocialLinks = () => (
       <OutboundLink
         href="https://github.com/meganesu"
         aria-label="Megan's GitHub profile"
+        className={linkStyles}
       >
-        <FontAwesomeIcon icon={faGithub} size="2x" color="white" />
+        <FontAwesomeIcon icon={faGithub} size="2x" className={iconStyles} />
       </OutboundLink>
     </li>
     <li>
       <OutboundLink
         href="https://twitter.com/meganesulli"
         aria-label="Megan's Twitter profile"
+        className={linkStyles}
       >
-        <FontAwesomeIcon icon={faTwitter} size="2x" color="white" />
+        <FontAwesomeIcon icon={faTwitter} size="2x" className={iconStyles} />
       </OutboundLink>
     </li>
     <li>
       <OutboundLink
         href="https://linkedin.com/in/meganesu"
         aria-label="Megan's LinkedIn profile"
+        className={linkStyles}
       >
-        <FontAwesomeIcon icon={faLinkedin} size="2x" color="white" />
+        <FontAwesomeIcon icon={faLinkedin} size="2x" className={iconStyles} />
       </OutboundLink>
     </li>
   </ul>
