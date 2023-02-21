73b3e8ece033e256057a4be80dc1e3b3d2aecd70
Megan Sullivan <meganesulli@gmail.com>
2023-02-20 23:14:24 -0800

    feat(#107): extend CalloutBox to take color prop to change background color

--- src/components/callout-box/index.js
+++ src/components/callout-box/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import { container as containerStyles } from './styles.module.css'
+import {
+  container as containerStyles,
+  blue as blueStyles,
+  orange as orangeStyles,
+} from './styles.module.css'
 
-const CalloutBox = ({ children }) => {
+const CalloutBox = ({ children, color = "blue" }) => {
+  let colorClassName;
+  switch (color) {
+    case "blue":
+      colorClassName = blueStyles
+      break
+    case "orange":
+      colorClassName = orangeStyles
+      break
+    default:
+      colorClassName = blueStyles
+  }
+
   return (
-    <aside className={containerStyles}>
+    <aside className={`${containerStyles} ${colorClassName}`}>
       {children}
     </aside>
   )
