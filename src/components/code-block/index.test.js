c6439366af540badf12116cdeb0d69470f88dcf8
Megan Sullivan <3609989+meganesu@users.noreply.github.com>
2024-06-18 23:33:48 -0700

    feat(#62): add support for highlight-line comments

--- src/components/code-block/index.test.js
+++ src/components/code-block/index.test.js
@@ -49,3 +49,51 @@ describe('CodeBlock component', () => {
     expect(element).toBeDefined()
   });
 });
+
+describe('CodeBlock component with line highlighting', () => {
+  
+  it('supports highlight-line', async () => {
+    // Given an MDX code block using highlight-line
+    const mdFencedCodeBlock = `
+    \`\`\`javascript
+    const greeting = "Howdy" // highlight-line
+    
+    const name = "Megan" // highlight-line
+    console.log(greeting, ", my name is ", name) // highlight-line
+    
+    return true
+    \`\`\`
+    `
+    
+    let MdxContent
+    const mdxModule = await evaluate(
+      mdFencedCodeBlock,
+      {
+        ...runtime,
+        development: false,
+        remarkPlugins: [remarkMdxCodeMeta],
+      }
+    )
+      
+    MdxContent = mdxModule.default
+
+    // When the MDX code block is rendered
+    render(
+      <MdxContent components={{pre: CodeBlock}} />
+    )
+
+    // Then the highlight class is applied to the correct lines
+    const codeElements = screen.getAllByRole("code")
+
+    expect(codeElements[0].classList.contains("highlight")).toBe(true)
+    expect(codeElements[1].classList.contains("highlight")).toBe(false)
+    expect(codeElements[2].classList.contains("highlight")).toBe(true)
+    expect(codeElements[3].classList.contains("highlight")).toBe(true)
+    expect(codeElements[4].classList.contains("highlight")).toBe(false)
+    expect(codeElements[5].classList.contains("highlight")).toBe(false)
+  })
+
+  it.todo('supports highlight-start and highlight-end')
+
+  it.todo('supports highlight-next-line')
+})
